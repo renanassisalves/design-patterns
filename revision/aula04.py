593c76531533f19ed43ed69c5de927a14da1d09d
Renan de Assis Alves <renan.assis.alves@hotmail.com>
2022-04-25 02:31:17 -0300

    Add singleton pattern

---
+++ revision/aula04.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from uuid import uuid4
+
+class Pessoa(ABC):
+    def __init__(self: object, nome: str) -> None:
+        self.__nome = nome
+
+        @abstractmethod
+        def ganhar_dinheiro(self: object, ) -> None:
+            pass
+
+class Aluno(Pessoa):
+    def __init__(self: object, nome: str) -> None:
+        super().__init__(nome)
+        self.__matricula = str(uuid4()).split('-')[0].upper()
+
+    def ganhar_dinheiro(self: object) -> None:
+        print("Como ganhar dinheiro?")
+
+aluno1 = Aluno("Felicity")
+print(aluno1.__dict__)
+aluno1.ganhar_dinheiro()
