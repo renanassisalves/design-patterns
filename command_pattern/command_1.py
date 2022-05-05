e1a0084180db09ac77295d5c22854eb49cb1ccbb
Renan de Assis Alves <renan.assis.alves@hotmail.com>
2022-05-05 00:10:21 -0300

    Add command pattern

---
+++ command_pattern/command_1.py
@@ -0,0 +1,25 @@
+class Instalador:
+    def __init__(self, fonte, destino):
+        self.opcoes = []
+        self.destino = destino
+        self.fonte = fonte
+
+    def preferencias(self, escolha):
+        self.opcoes.append(escolha)
+
+    def executar(self):
+        for opcao in self.opcoes:
+            if list(opcao.values())[0]:
+                print(f"Copiando os binários de {self.fonte} para {self.destino}")
+            else:
+                print(f"Operação finalizada")
+
+if __name__ == "__main__":
+    # Inicia o instalador
+    instalador = Instalador("python3.9.1.gzip", "/usr/bin/")
+
+    # O usuário escolha instalar apenas o Python
+    instalador.preferencias({"python":True})
+    instalador.preferencias({"java":False})
+
+    instalador.executar()
