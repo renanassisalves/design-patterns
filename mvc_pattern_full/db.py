80e4bef2949864047bb2dc16c7b9e0d5c8682515
Renan de Assis Alves <renan.assis.alves@hotmail.com>
2022-05-10 01:40:32 -0300

    Add MVC pattern

---
+++ mvc_pattern_full/db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+def _executar(query):
+    db_path = "./geek.university"
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    resultado = None
+
+    try:
+        cursor.execute(query)
+        resultado = cursor.fetchall()
+        connection.commit()
+    except Exception as err:
+        print(f"Erro na execução da query: {err}")
+    connection.close()
+
+    return resultado
+
+"""
+resultado = [(1, "Playstation 5 ", 1234, 1), )]
+"""
